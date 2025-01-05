36f75f56b14fe753245e8b8c86dfe3d841114826
tom <tomasman1979@gmail.com>
2025-01-05 12:11:31 +0100

    New Page model

--- homer/models.py
+++ homer/models.py
@@ -1,5 +1,8 @@
+from datetime import datetime
 from typing import Optional
+from urllib import parse
 
+from markdown import markdown
 import sqlalchemy as sqa
 import sqlalchemy.orm as sqo
 from flask_login import UserMixin
@@ -27,3 +30,42 @@ class User(UserMixin, db.Model):
 @login.user_loader
 def load_user(id):
     return db.session.get(User, int(id))
+
+
+class Page(db.Model):
+    id: sqo.Mapped[int] = sqo.mapped_column(primary_key=True)
+    title: sqo.Mapped[str] = sqo.mapped_column(sqa.String(140))
+    url_suffix: sqo.Mapped[str] = sqo.mapped_column(sqa.String(30), unique=True)
+    body: sqo.Mapped[str] = sqo.mapped_column(sqa.Text)
+    body_html: sqo.Mapped[str] = sqo.mapped_column(sqa.Text)
+    author_id: sqo.Mapped[int] = sqo.mapped_column(sqa.ForeignKey(User.id))
+    last_edited: sqo.Mapped[datetime] = sqo.mapped_column(
+        sqa.DateTime, default=datetime.now
+    )
+    last_edit_by: sqo.Mapped[int] = sqo.mapped_column(sqa.ForeignKey(User.id))
+
+    def __repr__(self):
+        return "<Page {}>".format(self.title)
+
+    @sqo.validates("author_id")
+    def validate_author(self, key, value):
+        if self.author_id and self.author_id != value:
+            raise ValueError("Nelze měnit autora.")
+        return value
+
+    @sqo.validates("url_suffix")
+    def validate_url_suffix(self, key, value):
+        if value != parse.quote(value):
+            raise ValueError("URL část musí být url-safe.")
+        if len(value) > 30:
+            raise ValueError("URL část může být maximálně 30 znaků dlouhá.")
+        if "/" in value:
+            raise ValueError("URL část nesmí obsahovat lomítka.")
+        return value
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        target.body_html = markdown(value, output_format="html")
+
+
+db.event.listen(Page.body, "set", Page.on_changed_body)

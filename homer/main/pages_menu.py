49124ad562d9a06db0b5e82d49f6b616986f185c
tom <tomasman1979@gmail.com>
2025-01-18 18:36:59 +0100

    Reduce menu creation memory footprint

--- homer/main/pages_menu.py
+++ homer/main/pages_menu.py
@@ -3,7 +3,10 @@ from homer.models import Page
 
 def inject_navigation(db):
     def _inject_navigation():
-        pages = sorted(db.session.query(Page).all(), key=lambda p: p.url_suffix)
+        pages = sorted(
+            db.session.query(Page).with_entities(Page.url_suffix).all(),
+            key=lambda p: p.url_suffix,
+        )
         return dict(pages=pages)
 
     return _inject_navigation
